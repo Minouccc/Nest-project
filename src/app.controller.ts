310a50cdfda987c294c2aaed2453a9b6199b4a75
Minouccc <343723899@qq.com>
2024-06-24 17:03:32 +0800

    feat(用户管理模块): 配置抽离、登录认证鉴权

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission, UserInfo } from './custom.decorator';
+import { userInfo } from 'os';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('aaa')
+  @RequireLogin()
+  @RequirePermission('ddd')
+  aaa(@UserInfo('username') username: string, @UserInfo() userInfo) {
+    console.log(username);
+    console.log(userInfo);
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  bbb() {
+    return 'bbb';
   }
 }
